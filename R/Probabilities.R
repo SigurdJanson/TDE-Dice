0f10583397f09f443137fdc6777c75cd15650798
Jan <59577271+SigurdJanson@users.noreply.github.com>
2026-02-08 17:19:43 +0100

    Function for the distribution of the sum of 3 d20

--- R/Probabilities.R
+++ R/Probabilities.R
@@ -68,3 +68,25 @@ pAttr <- function(eav) {
     )
   )
 }
+#
+# SKILLS ###################
+#
+
+#' The density function for rolling a sum of 3d20.
+#' @param x A vector of dice sums.
+#' @value The vector starts with indices 1 and 2, both of which are zero because
+#' 3d20 has a sum of at least 3.
+d3D20 <- function(x) {
+  p <- c(0, 0, 1, 3, 6, 10, 15, 21, 28,
+         36, 45, 55, 66, 78, 91, 105, 120, 136, 153,
+         171, 190, 210, 228, 244, 258, 270, 280, 288, 294,
+         298, 300, 300, 298, 294, 288, 280, 270, 258, 244,
+         228, 210, 190, 171, 153, 136, 120, 105, 91, 78,
+         66, 55, 45, 36, 28, 21, 15, 10, 6, 3, 1)
+
+  if (missing(x)) return(p)
+
+  x[x < 3L | x > 60L] <- 1L # handle indices of p that are out of range
+  return(p[x] / 8000)
+}
+
---
+++ tests/testthat/test_d3D20.R
@@ -0,0 +1,25 @@
+test_that("missing parameter returns the whole distribution", {
+  expect_length(d3D20(), length(1:60))
+})
+
+test_that("NULL parameter returns an empty set", {
+  result <- d3D20(NULL)
+  expect_length(result, 0L)
+  expect_type(result, "double")
+})
+
+test_that("known values give the correct result", {
+  sums <- c(3, 8, 17, 25, 30)
+  # taken from https://www.omnicalculator.com/statistics/dice
+  expected <- c(0.000125, 0.002625, 0.015, 0.03225, 0.03725)
+
+  expect_equal(d3D20(sums), expected)
+})
+
+test_that("Distribution is symmetric", {
+  front <- 3:31
+  back  <- 32:60
+
+  result <- d3D20()
+  expect_equal(result[front], rev(result[back]))
+})
