f5e3f77ed67ccc18d28a846e8891f5a6f948d2ba
Jan <59577271+SigurdJanson@users.noreply.github.com>
2026-02-10 00:34:07 +0100

    Moved the breakpoints  to a global variable

--- R/helpers.R
+++ R/helpers.R
@@ -1,9 +1,14 @@
+#' The break points needed to determine the quality level based on
+#' a number of skill points.
+qualityLevelBreakPoints <- c(4L, 7L, 10L, 13L, 16L)
+
+
 #' Determine the quality level based on available skill points.
 #' @param sp Skill points (integer)
 #' @details This function does not validate its arguments.
 #' It's been tested for a value range of [-20, 50].
 qualityLevel <- function(sp)
   ifelse(sp >= 0L,
-    findInterval(sp, vec = c(4L, 7L, 10L, 13L, 16L)) + 1L,
+    findInterval(sp, vec = qualityLevelBreakPoints) + 1L,
     0L)
 
