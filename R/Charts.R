5babc5b4afce3f34f223591e8e93079ea1ec9c20
Jan <59577271+SigurdJanson@users.noreply.github.com>
2026-05-25 22:28:05 +0200

    Added method `plotAttributeChecks`

--- R/Charts.R
+++ R/Charts.R
@@ -1,13 +1,21 @@
 # CHARTS
 
+
+#' Creates a plot that shows the probabilities of attribute checks.
+#'
+#' @returns A ggplot object
+#' @export
+#'
+#' @examples
+#' plotAttributeChecks()
 plotAttributeChecks <- function() {
   require(ggplot2)
   ylabels <- \(x) paste(x, "%")
 
-  df <- pAttr(1:24) |>
+  df <- cAttr(1:24) |>
     as.data.frame() |>
     cbind(EAV = 1L:24L) |>
-    reshape(idvar = "EAV", varying = list(1:4), v.names = "p", timevar = "Outcome", direction = "long") #View()
+    reshape(idvar = "EAV", varying = list(1:4), v.names = "p", timevar = "Outcome", direction = "long")
   df$Outcome <- as.factor(df$Outcome)
   df$p <- df$p * 100
 
@@ -15,7 +23,7 @@ plotAttributeChecks <- function() {
     geom_point() +
     geom_line() +
     guides(color = guide_legend(title = "Outcome")) +
-    scale_color_manual(values = c("chartreuse4", "aquamarine", "salmon1", "red4"),
+    scale_color_manual(values = c("#00876c", "#78ab63", "#e18745", "#d43d51"),
                        labels = c("Critical", "Success", "Fail", "Botch")) +
     scale_x_continuous(breaks = seq(0, 25, by = 10)) +
     scale_y_continuous(breaks = seq(0, 100, by = 10),
