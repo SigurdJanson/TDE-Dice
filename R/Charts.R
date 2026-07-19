4cc01df1c678500535f2452bdf1e03e3e2ad4975
Jan <59577271+SigurdJanson@users.noreply.github.com>
2026-07-19 15:18:03 +0200

    Added Skill Remainder Plot

--- R/Charts.R
+++ R/Charts.R
@@ -1,4 +1,5 @@
-# CHARTS
+library(ggplot2)
+
 
 
 #' Creates a plot that shows the probabilities of attribute checks.
@@ -32,3 +33,42 @@ plotAttributeChecks <- function() {
     ylab("Probability") +
     theme_minimal()
 }
+
+
+#' Skill Check Plots
+#'
+#' Plot the probabilities of a skill check. Outcomes are plotted on
+#' the x-axis with probabilities on y. Skill point remainders,
+#'
+#' @param eav A vector with three integers as effective attribute values.
+#' @param skill Skill level (integer, scalar)
+#'
+#' @returns A ggplot object
+#' @export
+#'
+#' @examples
+#' plotSkillRemainder(c(7, 10, 13), 8)
+plotSkillRemainder <- function(eav, skill) {
+  require(ggplot2)
+  .Palette <- c("#999999FF", # FAIL
+                "#1B641B", "#269C29", "#33D23B", "#68E170", # QL1 - 4
+                "#9EEEA6", "#D7F9DB", "#EDFFDB") # QL 5 - 7
+
+  sp <- dSkillPoints(-1:skill, eav, skill, "df")
+  sp$QL <- qualityLevel(sp$Remainder) |> .qlfactor()
+
+  ylabels <- \(x) paste(x, "%")
+  Title <- "Probabilities of Remaining Skill Points"
+  SubTitle <- paste("EAV:", paste(eav, sep="/"), paste("Skill:", skill))
+  Palette <- .Palette[1L:(max(ql) + 1L)]
+
+  ggplot(sp, aes(x = Remainder, y = p, fill = QL)) +
+    geom_col() +
+    scale_fill_manual(values = Palette) +
+    scale_y_continuous(labels = ylabels) +
+    xlab("Skill Remainder") +
+    ylab("Probability") +
+    labs(title = Title, subtitle = SubTitle) +
+    theme_minimal()
+}
+
